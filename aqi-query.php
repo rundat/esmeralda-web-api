5acf03f5e900e4838781d02d2c4cd3f29f4e447b
Julien Sagot <ju.sagot@gmail.com>
2014-09-18 20:24:49 +0200

    Save data on server, one file per day. If a day is requested and not available yet, fetch it from esmeralda-web and save it on local server. Everything merged into aqi-query.php.

--- aqi-query.php
+++ aqi-query.php
@@ -1,27 +1,101 @@
 <?php
 
-require 'config.php' ;
-
-$AQI = unserialize (file_get_contents ($AQI_DATA_FILE));
-
-function getConc ($station, $pollutant)
+function localDataURL ($year, $mon, $day)
 {
-    global $AQI ;
-    return $AQI [$station] [$pollutant] ;
+    return "data/$year$mon$day.dat" ;
 }
 
-function getBatchConc ($stations, $pollutants)
+/* Generate URL to fetch according to date, domain and pollutant. */
+function dataURL ($year, $mon, $day, $dom, $pol)
 {
-    global $AQI ;
-    $data = [];
+    return sprintf ('http://www.esmeralda-web.fr/esmeralda/FIGS/'
+                   .'%d/%02d/%d%02d%02d/GN15/conc_peak.%s.%s.txt',
+                    $year, $mon, $year, $mon, $day, $dom, $pol) ;
+}
+
+/* Get content return by url using curl, and retur it as string. */
+function fetchURL ($url)
+{
+    $ch = curl_init ($url) ;
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1) ;
+    $res = curl_exec ($ch) ;
+    curl_close ($ch) ;
+    return $res ;
+}
+
+/* Get raw data from esmeralda-web for a day / domain / pollutant.
+ * Parse the result and extract values. */
+function fetchSingleData ($year, $mon, $day, $dom, $pol)
+{
+    $raw = fetchURL (dataURL ($year, $mon, $day, $dom, $pol)) ;
+
+    foreach (explode ("\n", $raw) as $line)
+    {
+        preg_match ('/\s*([\w\d_]+)'
+                   .'\s+(\d+)\.\s+\d+'
+                   .'\s+(\d+)\.\s+\d+'
+                   .'\s+(\d+)\.\s+\d+'
+                   .'\s+(\d+)\.\s+\d+'
+                   .'\s*/',
+                    $line, $matches) ;
+        if (count ($matches) == 6)
+        {
+            $data [$matches[1]] = array_slice ($matches, 2) ;
+        }
+    }
+
+    return $data ;
+}
+
+/* Fetch data for all pollutants and all stations according a given day. */
+function fetchData ($year, $mon, $day)
+{
+    $pollutants = [ 'PM10', 'PM25', 'NO2', 'O3' ] ;
+    $domains = [ 'bour', 'bret', 'chp3', 'idf3',
+                 'lgr3', 'nmd', 'npc', 'pcd' ] ;
+
+    $data = [] ;
+
+    foreach ($domains as $dom)
+    {
+        foreach ($pollutants as $pol)
+        {
+            foreach (fetchSingleData ($year, $mon, $day, $dom, $pol)
+                as $station => $conc)
+            {
+                $data [$dom.'.'.$station] [$pol] = $conc ;
+            }
+        }
+    }
+
+    return $data ;
+}
+
+/* Fetch data and save them on the server. */
+function saveData ($year, $mon, $day)
+{
+    $data = fetchData ($year, $mon, $day) ;
+    $filename = localDataURL ($year, $mon, $day) ;
+    return file_put_contents ($filename, serialize ($data)) ;
+}
+
+function getData ($year, $mon, $day, $stations, $pollutants)
+{
+    $filename = localDataURL ($year, $mon, $day) ;
+    if (!file_exists ($filename)
+        && !saveData ($year, $mon, $day))
+    {
+        return NULL ;
+    }
+    $db = unserialize (file_get_contents ($filename));
+
     foreach ($stations as $station)
     {
         foreach ($pollutants as $pollutant)
         {
-            $conc = getConc ($station, $pollutant) ;
-            if ($conc != NULL)
+            if (!empty ($db [$station] [$pollutant]))
             {
-                $data [$station] [$pollutant] = $conc ;
+                $data [$station] [$pollutant] = $db [$station] [$pollutant] ;
             }
         }
     }
@@ -30,9 +104,11 @@ function getBatchConc ($stations, $pollutants)
 
 function main ()
 {
+    $date = date_parse_from_format ("Y-m-d", $_GET ['d']) ;
     $stations = explode(',', $_GET ['s']) ;
     $pollutants = explode(',', $_GET ['p']) ;
-    $data = getBatchConc ($stations, $pollutants) ;
+    $data = getData ($date['year'], $date['month'], $date['day'],
+                     $stations, $pollutants) ;
     $json = json_encode ($data) ;
     if ($json != NULL)
     {
